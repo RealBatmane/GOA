b8989969ae8485e1c28bddb8dea1b1f8c2d7599c
RealBatmane <nikalursmanadze@gmail.com>
2026-01-06 10:54:54 -0800

    GOA BEST

---
+++ level 38/classwork/classwork.py
@@ -0,0 +1,33 @@
+# 1)ახსენით კომენტარებით რა არის tuple და რით განსხვავდება სიისგან
+# tuples igive tvisebebiaq rac lists ubralod misi shecvla anu magalitad raimes chanacvleba sxva monacemta tipit ar shegvidzlia 
+
+# 2)შექმენი Tuple სადაც გექნება შენახული ადამიანის სახელები, შემდეგ გამოიტანე პირველი და ბოლო ელემენტის გარდა ყველა ელემენტი, სცადე slicing ის გამოყენება
+
+name= ("nikolozi","giorgi","aleqsandre","vano")
+
+print(name[1:-1])
+
+
+# 3) კომენტარებით ახსენით tuple ს თითოეული მეთოდი რაომელი რას აკეთებს
+
+# count itvlis tu ramdenjer meordeba erti konkretuli monacemta tipi tuples shi 
+
+#index roca chven mivutiteb index() brwyaleb shuashi listidan romelime elements gvetyvis tu meramdene index ze aris igi
+
+# 4) კომენტარებით ახსენით დეტალურად ყველაფერი რასაც დაწერთ (კოდი ახსენით)
+
+
+
+# 5)კომენტარებით ახსენით რა არის unpacking და მოიყვანეთ unpacking ზე 5 მაგალითი 
+
+#unpacking aris rodesac tuples shi myof mnishnvelobebs sxvadasxva cvladebs mivaniwebt
+
+name= (1,2,3,4,5)
+
+name1,name2,name3,name4,name5 = name
+
+print(name1) #mieniwa pirveli mnishnveloba 
+print(name2)  #mieniwa meore mnishnveloba
+print(name3) #mieniwa mesame mnishnveloba
+print(name4) #mieniwa meotxe mnishnveloba
+print(name5) #mieniwa mexute mnishnveloba
